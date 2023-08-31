72c412504a82453c227116f77f9ef24f93465ff7
vaibhavsaini9ine <138699713+vaibhavsaini9ine@users.noreply.github.com>
2023-08-31 05:41:14 +0530

    Add files via upload
    Task 3 Completed

---
+++ netfllinchi.py
@@ -0,0 +1,19 @@
+# Welcome 
+# This is Task 3 -- Netflix Clone
+# Lets Run the Code
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
